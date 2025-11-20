dc2715c536532e45dc8f1434dfe6f3c2ec7d3120
ortariot <ortariot@gmail.com>
2025-11-20 21:08:27 +0700

    fix copy pattern

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -36,16 +36,16 @@ class AuthService:
         return user
 
     def create_access_token(self, data: dict, expires_delta: timedelta = None):
-        to_encode = data.copy()
 
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
             expire = datetime.utcnow() + timedelta(minutes=15)
             
-        to_encode.update({"exp": expire})
+        data.update({"exp": expire})
+
         encoded_jwt = jwt.encode(
-            to_encode,
+            data,
             settings.auth.sekret_key,
             algorithm=settings.auth.algorithm,
         )
